02a13ca68bdfa1e31dfe57897a3f1ede5e4ffe23
Antonio Malara <antonio.malara@mediamonks.com>
2018-05-11 15:32:35 +0200

    Initial Commit

---
+++ pacmaschine/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pacmaschine
+//
+//  Created by Antonio Malara on 11/05/2018.
+//  Copyright © 2018 Antonio Malara. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
